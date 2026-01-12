7843d8cf1802fe1062a467834e914db55e4aa25c
MFluke876 <dr.catth@gmail.com>
2026-01-12 18:38:32 +0700

    Revert "Lovable update"
    This reverts commit 90795b902607682320ab7183aeb3d2c721e8c25f, reversing
    changes made to 69c52ca18c1e300343cafc18e9382479a1d67bd5.

--- supabase/migrations/20260112111248_e551c724-eea1-4eb3-a2ce-61740bb2b786.sql
+++
@@ -1,3 +0,0 @@
--- Remove the conflicting RESTRICTIVE policy that causes the RLS violation
--- Only staff should be able to register new patients
-DROP POLICY IF EXISTS "Authenticated users can insert patient record" ON public.patients;
